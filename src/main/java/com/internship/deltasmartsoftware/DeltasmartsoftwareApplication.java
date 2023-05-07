edf796b14761e6e695274fc4d8acdb2bf32afa03
ZaferSurucu <scream_boy.s@hotmail.com>
2023-05-07 22:41:36 +0300

    init
    Project setup.

---
+++ src/main/java/com/internship/deltasmartsoftware/DeltasmartsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.internship.deltasmartsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltasmartsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltasmartsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internship/deltasmartsoftware/DeltasmartsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.deltasmartsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltasmartsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
