392d701ea1c0914b25da25ffd467eb1e3cc55838
ZaferSurucu <scream_boy.s@hotmail.com>
2023-05-10 16:17:39 +0300

    demo rest controller added

---
+++ src/main/java/com/internship/deltasmartsoftware/rest/demorestcontroller.java
@@ -0,0 +1,13 @@
+package com.internship.deltasmartsoftware.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demorestcontroller {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is ";
+    }
+}
